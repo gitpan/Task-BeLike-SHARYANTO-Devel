681fde2960337c7778d2094934bf408fb03e379b
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-12 11:35:27 +0000

    Import of SHARYANTO/Task-BeLike-SHARYANTO-Devel-0.06 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-SHARYANTO-Devel
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SHARYANTO/Task-BeLike-SHARYANTO-Devel-0.06.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Task/BeLike/SHARYANTO/Devel.pm
+++ lib/Task/BeLike/SHARYANTO/Devel.pm
@@ -1,10 +1,9 @@
 package Task::BeLike::SHARYANTO::Devel;
 
-our $VERSION = '0.05'; # VERSION
+our $VERSION = '0.06'; # VERSION
 
 1;
 # ABSTRACT: Install modules currently used in SHARYANTO's developing environment
-
 
 
 __END__
@@ -16,7 +15,7 @@ Task::BeLike::SHARYANTO::Devel - Install modules currently used in SHARYANTO's d
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 DESCRIPTION
 
@@ -27,17 +26,25 @@ Not included are modules:
 
 =item * already in 5.10 core;
 
-=item * used in applications (see L<Task::BeLike::SHARYANTO::Apps>);
+=item * written by me (see L<Task::BeLike::SHARYANTO::Authored>);
 
-=item * required by my very specific applications;
-
-=item * required by my old/obsolete applications;
+=item * used in my applications (see L<Task::BeLike::SHARYANTO::Apps>);
 
 =back
 
 The purpose of this distribution is to make it easy to setup my developing
 environment. Thus, this distribution is probably only useful to me or people who
 want to imitate my developing environment.
+
+Todo/futures:
+
+=over 4
+
+=item * carton
+
+=item * Pinto
+
+=back
 
 =head1 TASK CONTENTS
 
@@ -51,15 +58,19 @@ want to imitate my developing environment.
 
 =head3 L<App::perlzonji>
 
+=head3 L<Carp::Always>
+
+=head3 L<Carp::Always::Color>
+
 =head3 L<Class::Autouse>
 
 =head3 L<CPAN::Mini>
 
-=head3 L<Devel::EndStats>
+=head3 L<Data::Dump>
+
+=head3 L<Devel::NYTProf>
 
 =head3 L<Dist::Zilla>
-
-=head3 L<Dist::Zilla::PluginBundle::SHARYANTO>
 
 =head3 L<Module::CoreList>
 
